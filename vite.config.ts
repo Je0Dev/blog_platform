aa0c993651fe1b894101664e0bdf9a36487b866b
giorgos_M000@hotmail.com <giorgos_M000@hotmail.com>
2026-04-07 14:36:36 +0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { inspectAttr } from 'kimi-plugin-inspect-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/blog_platform/',
   plugins: [inspectAttr(), react()],
   resolve: {
     alias: {
